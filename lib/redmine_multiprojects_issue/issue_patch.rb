5ff56ecf6c98e54e3f54e85cea308f82f0e510b9
Vincent Robert <vincent.robert@nanego.com>
2014-03-04 15:53:23 +0100

    Allow users to see issue if they have rights on at least one project linked to this issue

--- lib/redmine_multiprojects_issue/issue_patch.rb
+++ lib/redmine_multiprojects_issue/issue_patch.rb
@@ -4,4 +4,38 @@ class Issue
 
   has_and_belongs_to_many :projects
 
+  alias_method :core_visible?, :visible?
+
+  # Returns true if usr or current user is allowed to view the issue
+  def visible?(usr=nil)
+
+    other_projects = self.projects - [self.project]
+    other_projects_visibility = false
+
+    other_projects.each do |p|
+      if other_projects_visibility == false
+        other_projects_visibility = (usr || User.current).allowed_to?(:view_issues, p) do |role, user|
+          if user.logged?
+            case role.issues_visibility
+              when 'all'
+                true
+              when 'default'
+                !self.is_private? || (self.author == user || user.is_or_belongs_to?(assigned_to))
+              when 'own'
+                self.author == user || user.is_or_belongs_to?(assigned_to)
+              else
+                false
+            end
+          else
+            !self.is_private?
+          end
+        end
+      else
+        break
+      end
+    end
+
+    core_visible? || other_projects_visibility
+  end
+
 end
--- lib/redmine_multiprojects_issue/project_patch.rb
+++ lib/redmine_multiprojects_issue/project_patch.rb
@@ -7,7 +7,7 @@ class Project
   def self.allowed_to_condition(user, permission, options={})
 
     # TODO Improve the request to limit it according to permissions
-    "(#{core_allowed_to_condition(user, permission, options)} OR #{Project.table_name}.id IN (SELECT project_id FROM issues_projects GROUP BY project_id))"
+    "(#{core_allowed_to_condition(user, permission, options)} OR #{Project.table_name}.id IN (SELECT project_id FROM issues_projects WHERE issue_id IN (SELECT issue_id FROM issues_projects GROUP BY issue_id HAVING count(issue_id) > 1) GROUP BY project_id))"
 
   end
 
