f8eb36964b905fc501d9f3834dbd6887e27e4a9b
Vincent Robert <vincent.robert@nanego.com>
2014-03-10 10:15:47 +0100

    Bug fix - Patch Query model only if the query is an IssueQuery

--- lib/redmine_multiprojects_issue/query_patch.rb
+++ lib/redmine_multiprojects_issue/query_patch.rb
@@ -5,14 +5,36 @@ class Query
   alias_method :core_project_statement, :project_statement
 
   def project_statement
+
     project_clauses = core_project_statement
 
-    if project_clauses
-      "((#{project_clauses}) OR #{Issue.table_name}.id IN (SELECT issue_id FROM issues_projects WHERE project_id = #{project.id}))"
+    if self.is_a?(IssueQuery)
+      if project_clauses
+        "((#{project_clauses}) OR #{Issue.table_name}.id IN (SELECT issue_id FROM issues_projects WHERE project_id = #{project.id}))"
+      else
+        nil
+      end
     else
-      nil
+      project_clauses
     end
 
+  end
+
+end
+
+require_dependency 'issue_query'
+
+class IssueQuery < Query
+
+  # Returns the versions
+  # Valid options are :conditions
+  def versions(options={})
+    Version.visible.where(options[:conditions]).all(
+        :include => :project,
+        :conditions => core_project_statement
+    )
+  rescue ::ActiveRecord::StatementInvalid => e
+    raise StatementInvalid.new(e.message)
   end
 
 end
