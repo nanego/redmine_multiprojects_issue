2a456e1200f7e83e83b978c2d9546e0b0839f07b
Vincent Robert <vincent.robert@nanego.com>
2014-03-12 15:10:12 +0100

    Hide sidebar when the user cannot use it

--- lib/redmine_multiprojects_issue/application_helper_patch.rb
+++ lib/redmine_multiprojects_issue/application_helper_patch.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   alias_method :core_link_to_project, :link_to_project
 
   def link_to_project(project, options={}, html_options = nil)
-    if (project.is_public? || User.current.present? && (User.current.admin? || User.current.member_of?(project) ))
+    if (project.is_public? || User.current.admin? || User.current.member_of?(project))
       core_link_to_project(project, options, html_options)
     else
       h(project.name)
