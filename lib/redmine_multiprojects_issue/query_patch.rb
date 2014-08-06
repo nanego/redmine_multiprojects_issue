1229e328aeb27bd04727f7546e3cf331435be6de
Vincent Robert <vincent.robert@nanego.com>
2014-08-06 14:59:41 +0200

    Improve reliability by using alias_method_chain instead of alias_method

--- lib/redmine_multiprojects_issue/query_patch.rb
+++ lib/redmine_multiprojects_issue/query_patch.rb
@@ -2,32 +2,28 @@ require_dependency 'query'
 require_dependency 'issue_query'
 
 class Query
-
-  alias_method :core_project_statement, :project_statement
-
-  def project_statement
-
-    project_clauses = core_project_statement
-
-    if self.is_a?(IssueQuery)
-      if project_clauses
-        "((#{project_clauses}) OR #{Issue.table_name}.id IN (SELECT issue_id FROM issues_projects WHERE project_id = #{project.id}))"
+  unless instance_methods.include?(:project_statement_with_multiproject_issues)
+    def project_statement_with_multiproject_issues
+      project_clauses = project_statement_without_multiproject_issues
+      if self.is_a?(IssueQuery)
+        if project_clauses
+          "((#{project_clauses}) OR #{Issue.table_name}.id IN (SELECT issue_id FROM issues_projects WHERE project_id = #{project.id}))"
+        else
+          nil
+        end
       else
-        nil
+        project_clauses
       end
-    else
-      project_clauses
     end
-
+    alias_method_chain :project_statement, :multiproject_issues
   end
-
 end
 
 class IssueQuery < Query
   # Returns the versions, bypassing the project_statement method patched above
   def versions(options={})
     Version.visible.
-        where(core_project_statement).  # bypass patched method
+        where(project_statement_without_multiproject_issues).  # bypass patched method
         where(options[:conditions]).
         includes(:project).
         all
