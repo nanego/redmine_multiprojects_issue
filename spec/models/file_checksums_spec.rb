a09a45ffc64d8fea4b4338020292f375e7765376
Vincent Robert <vincent.robert@nanego.com>
2015-09-22 14:19:59 +0200

    Update checksums in specs

--- spec/models/file_checksums_spec.rb
+++ spec/models/file_checksums_spec.rb
@@ -7,19 +7,21 @@ describe "FileChecksums" do
     checksum = Digest::MD5.hexdigest(File.read(filepath))
     assert checksum.in?(Array(expected)), "Bad checksum for file: #{filename}, local version should be reviewed: checksum=#{checksum}, expected=#{Array(expected).join(" or ")}"
   end
-
   it "should core issue model checksum" do
     # "notified_users", "visible_condition" and "visible?" methods are overridden
     # and should be reviewed if this test breaks
     # 2.3.3 is ok
     # 2.5.1 and 2.5.2 are ok
     # 2.6.0 is ok
-    # 3.0.3 is ok
+    # 3.0.3 and 3.1.0 are ok
+    # 3.1.1 is ok
     assert_checksum %w(7265e9dcb488ac8ea89ed7c3c92c8c88
                        31b05314384ac3bbe273ae9a0b0f7e24
                        f93d75a3bb8f60360e957d9335c15f43
                        2ebfdef98d98062124c77cc12f61519f
-                       0f2bcd7050d5383ce2884f84a3304170), "app/models/issue.rb"
+                       5c85983c6b8ddb52fe4656d8e5d2471c,
+                       a5a2fa1156016e4380e63d9f75c3bd75,
+                       157f0a0c68e90e302ae565ac3cdb2ed7), "app/models/issue.rb"
   end
 
   # tests have been added to the issue_helper_patch_test file, no need to check the checksum
@@ -31,21 +33,24 @@ describe "FileChecksums" do
   it "should core query model checksum" do
     # "project_statement" method is overridden
     # and should be reviewed if this test breaks
-    # 2.3.3, 2.5.1, 2.6.0 and 3.0.3 are ok
+    # 2.3.3, 2.5.1, 2.6.0, 3.0.3 and 3.1.0 are ok
     assert_checksum %w(4c224b8cf3777fe2708139cfa77684eb
                        01fe00cf132446c39f0485148ddaf8f7
                        cb76a735b8d9304cadf1fc40e641dc7e
-                       10a67f765cfc83a1fcea2d2586006ef8), "app/models/query.rb"
+                       5db60e7361522d102722fbcff95494e5
+                       63238edafc1bf5c83ec3da125eb5b776), "app/models/query.rb"
   end
 
   it "should core issue query model checksum" do
     # "versions" method is overridden
     # and should be reviewed if this test breaks
-    # 2.3.3, 2.5.1, 2.6.0 and 3.0.3 are ok
+    # 2.3.3, 2.5.1, 2.6.0, 3.0.3, 3.1.0 and 3.1.1 are ok
     assert_checksum %w(640fbc448f8d90093d05685aa4292893
                        6c1ba80a5f4e22680ecb946a6509599a
                        683cedbb78c368325997a6fe11d27d5c
-                       53ab195fad836aae2ad7f6aeb273ca17), "app/models/issue_query.rb"
+                       53ab195fad836aae2ad7f6aeb273ca17
+                       6a6600f344e389682a8d51a881e089b7,
+                       949d5cdd375d4247a061fb004d90a81f), "app/models/issue_query.rb"
   end
 
   it "should core edit and new form js checksum" do
