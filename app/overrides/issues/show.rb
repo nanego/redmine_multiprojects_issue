00faafc1723fa8e818d4673de9195e8730ad8979
Ben <uaqben@opmbx.org>
2016-06-07 15:42:55 +0200

    Moving the render so that 'Related projects' be listed right after redmine's default 'Related issues'

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -1,5 +1,6 @@
 Deface::Override.new :virtual_path => 'issues/show',
                      :original     => 'ee65ebb813ba3bbf55bc8dc6279f431dbb405c48',
                      :name         => 'show-projects-in-issue-description',
-                     :insert_after => '.attributes',
+                     #:insert_after => '.attributes',
+                     :insert_after => '#relations',
                      :partial         => 'issues/show_projects'
