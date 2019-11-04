7827777e0c746051e5241ac90c19cbd32c36bdd5
Vincent Robert <vincent.robert@nanego.com>
2019-11-04 11:46:13 +0100

    Improve compatibility with redmine_customize_core_fields plugin

--- app/overrides/issues/_form.rb
+++ app/overrides/issues/_form.rb
@@ -2,4 +2,10 @@ Deface::Override.new :virtual_path => 'issues/_form',
                      :original     => 'ee65ebb813ba3bbf55bc8dc6279f431dbb405c48',
                      :name         => 'add-multiple-projects-to-issue-form',
                      :insert_after => '.attributes',
-                     :partial         => 'issues/select_projects'
+                     :partial      => 'issues/select_projects'
+
+Deface::Override.new :virtual_path => 'issues/_form_with_positions',
+                     :original     => 'ee65ebb813ba3bbf55bc8dc6279f431dbb405c48',
+                     :name         => 'add-multiple-projects-to-issue-form',
+                     :insert_after => '.attributes',
+                     :partial      => 'issues/select_projects'
