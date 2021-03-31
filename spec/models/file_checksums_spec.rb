2bf85d4132f19d3090d4caa5083575a469e20461
Vincent Robert <vincent.robert@nanego.com>
2021-03-31 09:38:33 +0200

    Redmine 4.1.2 compatibility: check changes and update checksums in plugin specs

--- spec/models/file_checksums_spec.rb
+++ spec/models/file_checksums_spec.rb
@@ -10,8 +10,8 @@ describe "FileChecksums" do
   it "should core issue model checksum" do
     # "notified_users", "visible_condition" and "visible?" methods are overridden
     # and should be reviewed if this test breaks
-    # 4.0.0, 4.0.6 and 4.1.0 are ok
-    assert_checksum %w(71289d390f3afbd70f0e284382875e5d 7bf3334c6f62ec6c6baec7ca883b8019 0436c38146aea3ca0be29b9a89a41dc4), "app/models/issue.rb"
+    # 4.0.0, 4.0.6 and 4.1.2 are ok
+    assert_checksum %w(71289d390f3afbd70f0e284382875e5d 7bf3334c6f62ec6c6baec7ca883b8019 0436c38146aea3ca0be29b9a89a41dc4 656212bf71ea687247ef7e9736819901), "app/models/issue.rb"
   end
 
   # tests have been added to the issue_helper_patch_test file, no need to check the checksum
@@ -23,8 +23,8 @@ describe "FileChecksums" do
   it "should core query model checksum" do
     # "project_statement" method is overridden
     # and should be reviewed if this test breaks
-    # 4.0.0 and 4.1.1 are ok
-    assert_checksum %w(57279103006a6e28cc3fa5fedd51b637 9a8c112af47dc4f8401740e88bea52df bfdf56d03af9e4812bbc340edf86e427), "app/models/query.rb"
+    # 4.0 and 4.1 are ok
+    assert_checksum %w(57279103006a6e28cc3fa5fedd51b637 9a8c112af47dc4f8401740e88bea52df bfdf56d03af9e4812bbc340edf86e427 3f9a98e32dca8600372e5559e1b1d781), "app/models/query.rb"
   end
 
   it "should core issue query model checksum" do
