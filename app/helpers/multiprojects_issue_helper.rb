d56e3e7b4c653e82b958b8f26e37d38866f24bdd
Vincent Robert <vincent.robert@nanego.com>
2022-07-13 10:53:15 +0200

    Use hash to store query result

--- app/helpers/multiprojects_issue_helper.rb
+++ app/helpers/multiprojects_issue_helper.rb
@@ -1,14 +1,21 @@
 module MultiprojectsIssueHelper
   def custom_values_by_projects(projects, custom_fields)
     values_by_projects = {}
-    enumeration_values = CustomFieldEnumeration.select(:id, :name).joins(:custom_field).where(:custom_field_id => custom_fields.map(&:id)).to_a
+    enumeration_values = {}
+    CustomFieldEnumeration.select(:id, :name)
+                          .joins(:custom_field)
+                          .where(:custom_field_id => custom_fields.map(&:id))
+                          .order(:id)
+                          .each { |enum|
+                            enumeration_values[enum.id] = enum.name
+                          }
     projects.each do |project|
       values_by_projects.merge!(project.id => {})
       custom_fields.each do |custom_field|
         values = custom_field.custom_values.select { |cv| cv.customized_id == project.id }
         values.each do |custom_value|
           if custom_field.field_format == 'enumeration'
-            value = enumeration_values.find { |enum_value| enum_value.id == custom_value.value.to_i }.to_s
+            value = enumeration_values[custom_value.value.to_i]
           else
             value = custom_value.value
           end
