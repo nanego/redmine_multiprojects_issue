7159d4f387c244113ca0328caa891fd62a3c36d8
Vincent Robert <vincent.robert@nanego.com>
2023-01-16 09:44:24 +0100

    Remove optimization to prepare wider modifications

--- app/helpers/multiprojects_issue_helper.rb
+++ app/helpers/multiprojects_issue_helper.rb
@@ -1,21 +1,13 @@
 module MultiprojectsIssueHelper
   def custom_values_by_projects(projects, custom_fields)
     values_by_projects = {}
-    enumeration_values = {}
-    CustomFieldEnumeration.select(:id, :name)
-                          .joins(:custom_field)
-                          .where(:custom_field_id => custom_fields.map(&:id))
-                          .order(:id)
-                          .each { |enum|
-                            enumeration_values[enum.id] = enum.name
-                          }
     projects.each do |project|
       values_by_projects.merge!(project.id => {})
       custom_fields.each do |custom_field|
         values = custom_field.custom_values.select { |cv| cv.customized_id == project.id }
         values.each do |custom_value|
           if custom_field.field_format == 'enumeration'
-            value = enumeration_values[custom_value.value.to_i]
+            value = CustomFieldEnumeration.where(id: custom_value.value.to_i).first.to_s
           else
             value = custom_value.value
           end
