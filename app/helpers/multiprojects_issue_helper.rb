cc50feb0976648c10f22c0c7766c192eb79e9151
Yalaeddin <y.alaeddin@adacis.net>
2023-07-04 14:27:04 +0200

    Accélérer l'affichage de la liste de sélection des "autres projets concernés"

--- app/helpers/multiprojects_issue_helper.rb
+++ app/helpers/multiprojects_issue_helper.rb
@@ -17,4 +17,25 @@ module MultiprojectsIssueHelper
     end
     values_by_projects
   end
+
+  # This function simulate custom_values_by_projects, but projects are an array of attributes not activerecord
+  def custom_values_by_attributes_projects(projects, custom_fields)
+    values_by_projects = {}
+    projects.each do |project|
+      project_id = project[0]
+      values_by_projects.merge!(project_id => {})
+      custom_fields.each do |custom_field|
+        values = custom_field.custom_values.select { |cv| cv.customized_id == project_id }
+        values.each do |custom_value|
+          if custom_field.field_format == 'enumeration'
+            value = CustomFieldEnumeration.where(id: custom_value.value.to_i).first.to_s
+          else
+            value = custom_value.value
+          end
+          values_by_projects[project_id].merge!(custom_field.id => value) if value.present?
+        end
+      end
+    end
+    values_by_projects
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 RedmineApp::Application.routes.draw do
-  get :plugin_multiprojects_issue_load_projects_selection, :to => "issues#load_projects_selection"
+  post :plugin_multiprojects_issue_load_projects_selection, :to => "issues#load_projects_selection"
 end
