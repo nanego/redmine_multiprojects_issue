95710ba2cda41375c968727757501e1e1b027534
Yalaeddin <y.alaeddin@adacis.net>
2023-07-06 08:58:39 +0200

    Supprimer la méthode custom_values_by_projects

--- app/helpers/multiprojects_issue_helper.rb
+++ app/helpers/multiprojects_issue_helper.rb
@@ -1,24 +1,5 @@
 module MultiprojectsIssueHelper
-  def custom_values_by_projects(projects, custom_fields)
-    values_by_projects = {}
-    projects.each do |project|
-      values_by_projects.merge!(project.id => {})
-      custom_fields.each do |custom_field|
-        values = custom_field.custom_values.select { |cv| cv.customized_id == project.id }
-        values.each do |custom_value|
-          if custom_field.field_format == 'enumeration'
-            value = CustomFieldEnumeration.where(id: custom_value.value.to_i).first.to_s
-          else
-            value = custom_value.value
-          end
-          values_by_projects[project.id].merge!(custom_field.id => value) if value.present?
-        end
-      end
-    end
-    values_by_projects
-  end
 
-  # This function simulate custom_values_by_projects, but projects are an array of attributes not activerecord
   def custom_values_by_attributes_projects(projects, custom_fields)
     values_by_projects = {}
     projects.each do |project|
--- spec/helpers/multiprojects_issue_helper_spec.rb
+++ spec/helpers/multiprojects_issue_helper_spec.rb
@@ -3,14 +3,6 @@ require "spec_helper"
 describe "MultiprojectsIssueHelper" do
   include MultiprojectsIssueHelper
 
-  it "should custom_values_by_projects" do
-    values = custom_values_by_projects(Project.all, ProjectCustomField.all)
-    refute_nil values
-    assert_kind_of Hash, values
-    expect(values.size).to eq 6
-    assert_equal({3=>"Stable"}, values[Project.first.id])
-  end
-
   it "should custom_values_by_attributes_projects" do
     project_ids = Project.all.map(&:id)
     projects_arrray = Project.find(project_ids).pluck(:id, :name, :status, :lft, :rgt).to_a
