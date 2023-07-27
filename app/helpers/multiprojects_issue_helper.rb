596ad134151fc8071b3c997b513dab0e209ed018
Vincent Robert <vincent.robert@nanego.com>
2023-07-27 16:28:15 +0200

    Improve performances: cache custom-fields-values by project

--- app/helpers/multiprojects_issue_helper.rb
+++ app/helpers/multiprojects_issue_helper.rb
@@ -6,23 +6,25 @@ module MultiprojectsIssueHelper
 
     projects.each do |project|
       project_id = project[0]
-      values_by_projects.merge!(project_id => {})
-      custom_fields.each do |custom_field|
-        values = custom_field.custom_values.select { |cv| cv.customized_id == project_id }
-        values.each do |custom_value|
-          if custom_field.field_format == 'enumeration'
-            value_after_temp = enumerations_values_by_custom_fields[custom_field.id].select { |enum| enum[0] == custom_value.value.to_i }.first
-            value = value_after_temp.present? ? value_after_temp[1].to_s : ""
-            # value = CustomFieldEnumeration.where(id: custom_value.value.to_i).first.to_s # Previously generating N+1 query
-          else
-            value = custom_value.value
+      values_by_projects[project_id] = Rails.cache.fetch("multiprojects-plugin-custom_values_by_project_ids-#{project_id}-#{custom_fields.map(&:id)}") do
+        custom_fields_values = {}
+        custom_fields.each do |custom_field|
+          values = custom_field.custom_values.select { |cv| cv.customized_id == project_id }
+          values.each do |custom_value|
+            if custom_field.field_format == 'enumeration'
+              value_after_temp = enumerations_values_by_custom_fields[custom_field.id].select { |enum| enum[0] == custom_value.value.to_i }.first
+              value = value_after_temp.present? ? value_after_temp[1].to_s : ""
+              # value = CustomFieldEnumeration.where(id: custom_value.value.to_i).first.to_s # Previously generating N+1 query
+            else
+              value = custom_value.value
+            end
+            custom_fields_values.merge!(custom_field.id => value) if value.present?
           end
-          values_by_projects[project_id].merge!(custom_field.id => value) if value.present?
         end
+        custom_fields_values
       end
     end
     values_by_projects
-
   end
 
   def enumerations_values_by_custom_fields(custom_fields)
