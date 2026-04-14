461f909586900b69f4025f7b6d776276ed95860e
Vincent <contact@vincent-robert.com>
2026-04-14 13:37:00 +0200

    Performances improvement: reduce memcache access by grouping all projects data

--- app/helpers/multiprojects_issue_helper.rb
+++ app/helpers/multiprojects_issue_helper.rb
@@ -1,31 +1,34 @@
 module MultiprojectsIssueHelper
 
   def custom_values_by_project_ids(projects, custom_fields)
-    values_by_projects = {}
-    enumerations_values_by_custom_fields = enumerations_values_by_custom_fields(custom_fields)
+    return {} if projects.empty? || custom_fields.empty?
 
+    project_ids = projects.map { |p| p[0] }
+    enumerations_values = enumerations_values_by_custom_fields(custom_fields)
     invalidation_cache_key = Project.maximum(:updated_on).to_i
+    cache_key = ['multiprojects-plugin-custom_values_by_projects',
+                 custom_fields.map(&:id).join('-'),
+                 invalidation_cache_key].join('-')
 
-    projects.each do |project|
-      project_id = project[0]
-      values_by_projects[project_id] = Rails.cache.fetch("multiprojects-plugin-custom_values_by_project_ids-#{project_id}-#{custom_fields.map(&:id)}-#{invalidation_cache_key}") do
-        custom_fields_values = {}
-        custom_fields.each do |custom_field|
-          values = custom_field.custom_values.select { |cv| cv.customized_id == project_id }
-          values.each do |custom_value|
-            if custom_field.field_format == 'enumeration'
-              # Equivalent to : value = CustomFieldEnumeration.where(id: custom_value.value.to_i).first.to_s # Previously generating N+1 queries
-              value = enumerations_values_by_custom_fields[custom_field.id][custom_value.value.to_i]
-            else
-              value = custom_value.value
-            end
-            custom_fields_values.merge!(custom_field.id => value) if value.present?
+    all_values = Rails.cache.fetch(cache_key) do
+      map = Hash.new { |h, k| h[k] = {} }
+      CustomValue
+        .where(customized_type: 'Project', custom_field_id: custom_fields.map(&:id))
+        .each do |cv|
+          next if cv.value.blank?
+          cf = custom_fields.find { |f| f.id == cv.custom_field_id }
+          next unless cf
+          value = if cf.field_format == 'enumeration'
+            enumerations_values[cf.id][cv.value.to_i]
+          else
+            cv.value
           end
+          map[cv.customized_id][cv.custom_field_id] = value if value.present?
         end
-        custom_fields_values
-      end
+      map
     end
-    values_by_projects
+
+    project_ids.each_with_object({}) { |pid, h| h[pid] = all_values[pid] || {} }
   end
 
   def enumerations_values_by_custom_fields(custom_fields)
